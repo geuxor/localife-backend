272421e64c4b7476814808c0b64d70877b2dfd61
German B <geuxor@dr.com>
2021-08-14 17:40:22 +0200

    initial ready

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+require('dotenv').config();
 var app = express_1.default();
 app.get('/', function (req, res) {
     res.send('Welcome!');
