0fd08d5b13d8c142f5309789b5f2e638076f501a
German B <geuxor@dr.com>
2021-08-14 13:37:13 +0200

    initial

---
+++ server/server.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var app = express_1.default();
+app.get('/', function (req, res) {
+    res.send('Welcome!');
+});
+// Server setup
+app.listen(process.env.PORT, function () {
+    console.log('The application is listening '
+        + 'on port http://localhost:' + process.env.PORT);
+});
