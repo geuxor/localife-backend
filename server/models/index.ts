9588e3b5640cf0bbc28bf57d489946d7aa52428c
German B <geuxor@dr.com>
2021-08-15 00:48:41 +0200

    model conx

---
+++ server/models/index.ts
@@ -0,0 +1,38 @@
+'use strict';
+const { Sequelize, DataTypes } = require('sequelize');
+const fs = require('fs');
+const path = require('path');
+const db = {};
+require('dotenv').config()
+
+console.log('dbconx:                       🧹 connecting to database')
+const sequelize = new Sequelize(process.env.DBPG_DATABASE, process.env.DBPG_USER, process.env.DBPG_PASSWORD, {
+  host: process.env.DBPG_HOST,
+  dialect: 'postgres',
+  logging: false,
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  // operatorsAliases: false // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+});
+const files = fs.readdirSync(__dirname);
+
+for (let file of files) {
+  if (file !== 'index.js') {
+    const modelCreation = require(path.join(__dirname, file))
+    const model = modelCreation(sequelize, DataTypes)
+    db[model.name] = model;
+  }
+}
+
+for (const model in db) {
+  if (db[model].associate) db[model].associate(db);
+}
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
