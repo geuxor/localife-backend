69911518dcccb438aa2e6b918baeeb33d9adc2d5
German B <geuxor@dr.com>
2021-08-14 22:39:45 +0200

    typescript-config

--- server.ts
+++ server.ts
@@ -1,14 +1,24 @@
-import express from 'express';
-require('dotenv').config()
+import * as dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+dotenv.config();
+// require('dotenv').config()
 
 const app = express();
+// DNS Prefetch Control, Frameguard, Hide Powered-By, HSTS, IE No Open, Don't Sniff Mimetype, and XSS Filter.
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome!');
 })
 
 // Server setup
-app.listen(process.env.PORT, () => {
-  console.log('The application is listening '
-    + 'on port http://localhost:' + process.env.PORT);
+app.listen(process.env.SERVER_PORT, () => {
+  console.log('⚡️ Server listening http://localhost:' + process.env.SERVER_PORT + ' ==================================>>');
 })
+
+// const PORT: number = parseInt(process.env.SERVER_PORT as string, 10);
+// console.log(PORT)
