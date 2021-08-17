ef3d406f60a6a46859a3c4dedf540445c9a29319
German B <geuxor@dr.com>
2021-08-17 19:01:38 +0200

    mockdata with new controllers, models for users and experiences

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,9 +1,51 @@
 // const bcrypt = require('bcrypt');
 const db = require('../models/index')
+const { validateNewUser, validateOldUser } = require('../utils/validate.user')
+
 
 function logme() {
   console.log('controller:                   🎮 entering user.controller *************');
 }
+
+// const getStatus = async (req, res) => {
+//   logme()
+//   console.log('### getStatus');
+//   try {
+//     const user = req.user
+//     res.status(200).send(user);
+//   } catch (err) {
+//     console.log(err, '🐛 User not found' );
+//     res.status(404).send(false);
+//   }
+// };
+
+const getUsers = async (req, res) => {
+  logme()
+  console.log('getUsers');
+  try {
+    const users = await db.User.findAll();
+    console.log('   #', users.length, 'users found');
+    // console.log("All users:", JSON.stringify(users, null, 2));
+    users.forEach(m => console.log(m.email))
+    res.status(200).send(users);
+  } catch (err) {
+    console.log(err);
+    res.status(404).send({ err, message: '🐛 Users not found' });
+  }
+}
+
+const getUserProfile = async (req, res) => {
+  logme()
+  console.log('getuserProfile');
+  try {
+    const user = req.user
+    const {email, firstname, lastname, createdAt} = user
+    res.status(200).send({ email, firstname, lastname, createdAt });
+  } catch {
+    res.status(404).send({ error, message: '🐛 User not found' });
+  }
+
+};
 
 const addUser = async (req, res) => {
   logme()
@@ -16,10 +58,13 @@ const addUser = async (req, res) => {
       .status(409)
       .send({ error: '409', message: '🐛 User already exists' });
   try {
-    const resUser = await db.User.create(user);
-    console.log('addUser: newUser Created:', resUser.toJSON())
-    req.session.uid = resUser.id;
-    res.status(201).send(resUser);
+    console.log('addUser: will soon bcrypting hash');
+    const newUser = await validateNewUser(req.body)
+    console.log('addUser: creating Validated newUser:', newUser);
+    const user = await db.User.create(newUser);
+    console.log('addUser: newUser Created:', user.toJSON())
+    req.session.uid = user.id;
+    res.status(201).send(user);
   } catch (error) {
     console.log(error);
     res.status(400).send({ error, message: 'addUser: 🐛 Could not create user' });
@@ -37,12 +82,18 @@ const loginUser = async (req, res) => {
       returning: true,
       plain: true,
     });
+    // console.log(user.toJSON());
     
     if (!user) {
       console.log(user, 'not found in DB!!!');
       res.status(403).send('🐛 User not Found!');
     }
-    console.log(user.dataValues, 'email found in DB!!!') 
+    console.log(user.dataValues, 'email found in DB!!!') //user.toJSON().email, //[1].dataValues
+    // bcrypt.compare
+    const validatedPass = await validateOldUser(user, email, password)
+    if (!validatedPass) throw new Error('🐛 password is incorrect');
+    req.session.uid = user.id;
+    console.log('loginUser: validated ok!!', user.email);
     res.status(200).send({ email: user.email, firstname: user.firstname, lastname: user.lastname, createdAt: user.createdAt});
   } catch (error) {
     console.log(error);
@@ -54,8 +105,22 @@ const loginUser = async (req, res) => {
 
 
 const logoutUser = (req, res) => {
-  console.log('logoutUser:');
-  res.sendStatus(200)
+  console.log('logoutUser');
+  setTimeout(() => {
+    console.log('waiting...')
+  
+  req.session.destroy((error) => {
+    if (error) {
+      res
+        .status(500)
+        .send({ error, message: '🐛 Could not log out, please try again' });
+    } else {
+      res.clearCookie('sid');
+      console.log('sid destroyed!!');
+      res.sendStatus(200) //.send('CLEARED');
+    }
+  });
+  }, 1000);
 };
 
-module.exports = { addUser, loginUser, logoutUser };
+module.exports = { getUsers, addUser, loginUser, logoutUser, getUserProfile };
