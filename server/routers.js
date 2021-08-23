ef7805f83518b8b48cecdfd4b9f8bf9cc5522c38
German B <geuxor@dr.com>
2021-08-24 00:04:24 +0200

    bug-fix final

--- server/routers.js
+++ server/routers.js
@@ -2,60 +2,62 @@ const router = require('express').Router();
 const authMiddleware = require('./middleware/auth.mw');
 
 console.log(`\n💫 ROUTES..............................
-get             /search-result?location=Barcelona
-
+>>AUTH..........
 post            /register
-post            /login => cookie is created
-get             /logout => cookie is destroyed
-post            /profile       ==> relogin user
+post            /login          => cookie is created
+get             /logout         => cookie is destroyed
+post            /profile        => relogin user
 
-EXPERIENCES.....
-get             /fake/experiences/:amount => creates and stores new fake data in DB with specific amount
-post            /experiences/new      => create a new experience
-get             /experiences          => get all experiences
-get             /destroy-experiences  ==> empties the Experience table in DB
-get             /xps  ==> returns fake data without hitting the DB
+>>EXPERIENCES....
+get             /search-result?country=Spain  => get result for any query
+get             /fake/experiences/:amount     => create new fake data in DB
+post            /experiences/new              => create a new experience
+get             /experiences                  => get all experiences
+get             /destroy-experiences          => empties the Experience DB table
+get             /xps                          => get fake data without hitting DB
 
-USERS..........
-get             /fake/users/"amount" ==> creates and stores new fake data in DB with specific amount
-get             /destroy-users ==> empties the User table in DB
-get             /users         ==> get all users
+>>USERS..........
+get             /fake/users/"amount"          => stores fake data in DB
+get             /destroy-users                => empties the User DB table
+get             /users                        => get all users
 
-BOOKINGS.......
-get             /fake/bookings/:amount'
-get             /destroy-bookings
+>>BOOKINGS.......
+get             /fake/bookings/:amount'       => stores fake data in DB
+get             /destroy-bookings             => empties the Booking DB table
 
-get             /update-stripe
+>>STRIPE.........
+get             /update-stripe                => create provider with stripe acc.
 ..............................\n`);
 //faker
 const seedsExperiences = require('./seeds/experiences')
 router.get('/fake/experiences/:amount', seedsExperiences.addFakeExperience);
 router.get('/destroy-experiences', seedsExperiences.destroyAllExperiences)
 router.get('/update-stripe', seedsExperiences.updateStripe)
+
 const seedsUsers = require('./seeds/users')
 router.get('/fake/users/:amount', seedsUsers.addFakeUser);
 router.get('/destroy-users', seedsUsers.destroyAllUsers)
+
 const seedsBookings = require('./seeds/bookings')
 router.get('/fake/bookings/:amount', seedsBookings.addFakeBookings)
 router.get('/destroy-bookings', seedsBookings.destroyAllBookings)
-
 
 //auth routes
 const userController = require('./controllers/user.controller')
 router.post('/register', userController.addUser);
 router.post('/login', userController.loginUser);
-router.get('/users', userController.getUsers);
 router.get('/logout', userController.logoutUser);
 router.post('/profile', authMiddleware, userController.getUserProfile);
+router.get('/users', userController.getUsers);
 
 //experiences routes
 const experienceController = require('./controllers/experience.controller')
 router.post('/search-results', experienceController.searchResults);
-router.get('/experience/:id', experienceController.getOneExperiences);
 router.get('/experiences', experienceController.allExperiences);
-router.post('/experiences/mine', experienceController.mineExperiences);
-router.post('/experiences/new', experienceController.addExperience);
-router.post('/experiences/addmany', experienceController.addManyExperiences);
+router.get('/experience/:id', experienceController.getOneExperiences);
+router.post('/experiences/mine', authMiddleware, experienceController.mineExperiences);
+router.post('/experiences/new', authMiddleware, experienceController.addExperience);
+router.post('/experiences/addmany', authMiddleware, experienceController.addManyExperiences);
 
 //bookings routes
 const bookingController = require('./controllers/booking.controller')
