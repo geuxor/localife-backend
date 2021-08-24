95affabfe8f105673fe00e2bfe9926d6f15f58ac
will <william.jl.johnson@gmail.com>
2021-08-24 18:02:12 +0200

    feat: route for cloudinary + config

---
+++ middleware/auth.mw.js
@@ -0,0 +1,30 @@
+const db = require('../models/index')
+
+const authMiddleware = async (req, res, next) => {
+  console.log('authMW:.......................🚽 entering authMiddelWare')
+  
+  if (!req.session) {
+    return next(new Error('Redis was Unable to store Session'))
+  }
+  console.log('with session:', req.sessionID, 'and body', req.body);
+  if (req.session.isAuth) {
+    console.log('Logged in');
+  } else {
+    console.log('Not Logged in');
+  }
+
+  try {
+    const { isAuth } = req.session;
+    console.log('MW: isAuth ', isAuth);
+    if (!isAuth) throw new Error('not Authenticated');
+    const user = await db.User.findOne({ where: { id: isAuth }});    //and where { email: req.body }?
+    console.log('User.id', user.id)
+    if (!user) throw new Error();
+    req.user = user;
+    next();
+  } catch (err) {
+    res.status(401).send(err)
+  }
+};
+
+module.exports = authMiddleware;
---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const redis = require('redis')
+const session = require('express-session');
+let RedisStore = require('connect-redis')(session)
+let redisClient = redis.createClient()
+const router = require('./routers')
+const cors = require('cors');
+const { sequelize } = require('./models/index')
+const fakeXps = require('./seeds/fakeXps')
+require('dotenv').config()
+
+const corsConfig = {
+  origin: process.env.CLIENT_ORIGIN,
+  credentials: true,
+};
+
+app.use(cors(corsConfig));
+app.use(express.json());
+
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    name: 'sid%localife',
+    saveUninitialized: false,
+    resave: false,
+    secret: process.env.SECRET || '*&^%$£$%TYUJIKL?<HDTYUKO<MKNBFE£$R%T^Y&UJNBFW',
+    cookie: {
+      maxAge: 4000000000,
+      sameSite: true,
+      httpOnly: false,
+      // set secure and httponly =true in prod
+      secure: false,
+    },
+  })
+);
+redisClient.on('server: error', console.error)
+
+app.use(router);
+
+app.get('/xps/:amount', (req, res) => {
+  res.json(fakeXps(req.params));
+});
+
+app.get('*', (req, res) => {
+  res.status(404).send('server ERR:                   🌵 No Route found');
+});
+
+(async () => {
+  try {
+    await sequelize.sync({ alter: true });
+    console.log('server:                       💽 database synced')
+    app.listen(process.env.SERVER_PORT, (err) => {
+      if (err) {
+        console.log(`server ERR:           👽 Bad errors occuring! ${err}`);
+      } else {
+        console.log(`===========================   🛰️ Server listening on port ${process.env.SERVER_PORT}! =======================>>`); // eslint-disable-line no-console
+      }
+    })
+  } catch (err) {
+    console.log(err)
+  }
+})();
