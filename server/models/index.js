9a561e74429bc47952e160a260a9c7e35e0aa8df
German B <geuxor@dr.com>
2021-09-01 12:26:50 +0200

    refactor: changed sequelize conx to heroku env

--- server/models/index.js
+++ server/models/index.js
@@ -7,18 +7,22 @@ require('dotenv').config()
 
 console.log('dbconx:                       🌠 connecting to database')
 
-const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
-  host: process.env.PGHOST,
-  dialect: 'postgres',
-    logging: false,
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000
-  },
-  // operatorsAliases: false // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
-});
+//configuration for heroku
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+
+//configuration for localhost
+// const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+//   host: process.env.PGHOST,
+//   dialect: 'postgres',
+//     logging: false,
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     acquire: 30000,
+//     idle: 10000
+//   },
+//   // operatorsAliases: false // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+// });
 const files = fs.readdirSync(__dirname);
 
 for (let file of files) {
