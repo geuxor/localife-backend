7bbe3e4b9dce46e56f3a092e67f63a1264eee7f3
German B <geuxor@dr.com>
2021-09-02 21:15:00 +0200

    redis -reconfig

--- server/models/index.js
+++ server/models/index.js
@@ -14,7 +14,10 @@ if (process.env.NODE_ENV === 'production') {
   sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     dialectOptions: {
-      ssl: true
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
     }
   })
 }
