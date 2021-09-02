15706b67ad61ae669236620159be99cdd3b9f640
German B <geuxor@dr.com>
2021-09-02 20:53:45 +0200

    redis- heroku config

--- server/models/index.js
+++ server/models/index.js
@@ -7,27 +7,34 @@ require('dotenv').config()
 
 console.log('dbconx:                       🌠 connecting to database')
 
-//configuration for heroku
-const sequelize = new Sequelize(process.env.DATABASE_URL)
-
+console.log('YOU ARE NOW RUNNING =======> ', process.env.NODE_ENV)
+let sequelize;
+if (process.env.NODE_ENV === 'production') {
+  //configuration for heroku
+  sequelize = new Sequelize(process.env.DATABASE_URL)
+}
 //configuration for localhost
-// const sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
-//   host: process.env.PGHOST,
-//   dialect: 'postgres',
-//     logging: false,
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     acquire: 30000,
-//     idle: 10000
-//   },
-//   // operatorsAliases: false // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
-// });
+if (process.env.NODE_ENV === 'development') {
+  sequelize = new Sequelize(process.env.PGDATABASE, process.env.PGUSER, process.env.PGPASSWORD, {
+    host: process.env.PGHOST,
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    // operatorsAliases: false // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+  });
+}
+
+
 const files = fs.readdirSync(__dirname);
 
 for (let file of files) {
   if (file !== 'index.js') {
-    const modelCreation = require(path.join(__dirname, file)) 
+    const modelCreation = require(path.join(__dirname, file))
     const model = modelCreation(sequelize, DataTypes)
     db[model.name] = model;
   }
